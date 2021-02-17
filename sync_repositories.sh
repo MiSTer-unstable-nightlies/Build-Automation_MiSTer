2468c8566326bed1490003da1ea3faf809f73a6c
Jose BG <theypsilon@gmail.com>
2021-02-18 00:24:09 +0100

    Json.

--- sync_repositories.sh
+++ sync_repositories.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 export GITHUB_TOKEN="${GITHUB_TOKEN}"
-gh api orgs/:owner/repos
+gh api orgs/:owner/repos > repos.json
+
+gh release create "json" -p || true
+gh release upload "json" "repos.json" --clobber
